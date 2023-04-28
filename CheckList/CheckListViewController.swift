50ff4006befc8ab744722188c6b4e674990d8933
cemtaskin <cemtaskin82@gmail.com>
2023-04-28 15:06:45 +0300

    cellforRowAt method updated

--- CheckList/CheckListViewController.swift
+++ CheckList/CheckListViewController.swift
@@ -27,7 +27,30 @@ class CheckListViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CheckListItem",for:indexPath)
         
         let label =  cell.viewWithTag(1000) as! UILabel
+        
         label.text = "\(indexPath.row)"
+        
+        switch(indexPath.row%5){
+        
+            case 0:
+            label.text = label.text! +  "Walk the dog"
+                break
+            case 1:
+            label.text = label.text! + " Brush your teeth"
+                break
+            case 2:
+            label.text = label.text! + "Learn iOS development"
+                break
+            case 3:
+            label.text = label.text! + "Soccer Practice"
+                break
+            case 4:
+            label.text = label.text! + "Eat ice cream"
+                break
+            default:
+                break
+        }
+
         
         return cell
     }
